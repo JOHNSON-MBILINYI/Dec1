194093b4cb77575316245ed19b7eec89ae3ffd88
JOHNSON-MBILINYI <Keyjunior0061@gmail.com>
2025-04-28 08:49:18 +0300

    Update My project.cpp

--- My project.cpp
+++ My project.cpp
@@ -5,6 +5,7 @@ struct Student {
     char name[50];
     float marks;
 }
+
 void addStudent(struct Student students[], int *count);
 void displayStudents(struct Student students[], int count);
 void searchStudent(struct Student students[], int count);
